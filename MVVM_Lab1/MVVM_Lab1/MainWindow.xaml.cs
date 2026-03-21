db0bee35b3598e03d8c9bb05cf363ebc41d55ce8
Hyuse-15 <ferum558@gmail.com>
2026-03-21 08:50:27 +0300

    База для лр 2
    Добавлен комбобокс выбора языка

--- MVVM_Lab1/MVVM_Lab1/MainWindow.xaml.cs
+++ MVVM_Lab1/MVVM_Lab1/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MVVM_Lab1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            
+            InitializeComponent();
+        }
+
+        private void LanguageSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LanguageSelector.SelectedItem is ComboBoxItem selectedItem)
+            {
+                string languageCode = selectedItem.Tag.ToString();
+                ChangeLanguage(languageCode);
+            }
+        }
+
+        private void ChangeLanguage(string languageCode)
+        {
+            // Базовый метод, который будет переопределен в каждой ветке
+            // В каждой ветке реализация будет разной
         }
     }
 }
